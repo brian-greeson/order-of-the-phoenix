4e75a46a2d2be5a55e8527c0db80d86666583500
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-19 10:21:23 -0600

    Add house_members method to harryservice

--- app/servcies/harry_service.rb
+++ app/servcies/harry_service.rb
@@ -1,12 +1,16 @@
 class HarryService
   def house_members(house)
-    binding.pry
-    hittus_apius("/houses")
+    houses = hittus_apius("houses")
+    houses.find {|h| h[:name] == house}[:members]
+  end
+
+  def houses 
+    hittus_apius("houses")
   end
 
 private
   def conn
-    Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
+    Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
       f.adapter Faraday.default_adapter
       f.params[:key] = ENV["HP_API_KEY"]
     end
